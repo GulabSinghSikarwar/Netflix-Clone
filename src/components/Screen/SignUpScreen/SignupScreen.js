9ec077fc859ec3ff99d850225104f82e7b040b38
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2022-03-12 08:28:25 +0530

    Added Authentication

--- src/components/Screen/SignUpScreen/SignupScreen.js
+++ src/components/Screen/SignUpScreen/SignupScreen.js
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from './SignupScreen.module.css'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { onAuthStateChanged} from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../../firebase_file';
+// import { auth } from '../../../firebase_file';
 function SignupScreen() {
 
-    const register=(e)=>{
+    const app = initializeApp(firebaseConfig)
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const auth = getAuth()
+
+    useEffect(() => {
+
+         const status = onAuthStateChanged(auth , (user)=>{
+            if(user){ console.log(user);}
+        })
+        return status
+
+    }, [])
+
+    const register = (e) => {
         e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        createUserWithEmailAndPassword(auth, email, password).then((user) => {
+            alert("User Registration Successful ")
+            console.log(user);
+        }).catch((e) => {
+            console.log(e);
+        })
+
 
     }
-    const signIn=(event)=>{
+    const signIn = (event) => {
         event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(auth, email, password).then((user) => {
+            alert("User Login In Successful ")
+            console.log(user);
+        }).catch((e) => {
+            alert(e.message)
+            console.log(e);
+        })
+
 
 
     }
@@ -16,9 +56,9 @@ function SignupScreen() {
         <div className={styled.signupScreen}>
             <form>
                 <h1>Sign In</h1>
-                <input type={"email"} placeholder="email" />
-                <input type={"password"} placeholder="password " />
-                <button  onClick={signIn} type={"submit"}>Sign In</button>
+                <input type={"email"} ref={emailRef} placeholder="email" required />
+                <input type={"password"} ref={passwordRef} placeholder="password " required />
+                <button onClick={signIn} type={"submit"}>Sign In</button>
                 <h4>
                     <span className={styled.signupScreen_grey}> New to Netflix ?</span>
                     <span onClick={register} className={styled.signupScreen_signup_link}> Sign Up Now  </span></h4>
---
+++ src/firebase_file.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app'
+
+import { getAuth } from 'firebase/auth';
+// initializeApp 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBjnrBsO0QhHQgKcr_1k2gy9jJdkCQelXM",
+    authDomain: "netflix-clone-43133.firebaseapp.com",
+    projectId: "netflix-clone-43133",
+    storageBucket: "netflix-clone-43133.appspot.com",
+    messagingSenderId: "94392355596",
+    appId: "1:94392355596:web:bdb729d56dece0b32c5729",
+    measurementId: "G-7CJHFBZKTD"
+  }; 
+  
+
+
+  export default firebaseConfig ;
